14841177d69199bdcfc1320401ec112975c37a23
Adam Ludvig <hunludvig@gmail.com>
2018-10-13 11:55:09 +0300

    Initial commit from Spring Initializr

---
+++ src/test/java/fi/hunludvig/aiven/KafkaPostgresAdaptor/KafkaPostgresAdaptorApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.hunludvig.aiven.KafkaPostgresAdaptor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaPostgresAdaptorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
