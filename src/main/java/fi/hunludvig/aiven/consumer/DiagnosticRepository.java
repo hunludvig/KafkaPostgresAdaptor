1f8632a91c47a440994739702e7410378fce1c9a
Adam Ludvig <hunludvig@gmail.com>
2018-10-14 16:21:10 +0300

    Move repository to consumer package

--- src/main/java/fi/hunludvig/aiven/consumer/DiagnosticRepository.java
+++ src/main/java/fi/hunludvig/aiven/consumer/DiagnosticRepository.java
@@ -1,6 +1,8 @@
-package fi.hunludvig.aiven.repository;
+package fi.hunludvig.aiven.consumer;
 
 import fi.hunludvig.aiven.model.Diagnostic;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface DiagnosticRepository extends CrudRepository<Diagnostic, Long> { }
